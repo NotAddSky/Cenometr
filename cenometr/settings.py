2a7e07e5fdd7d8821a6842b617583e82c21f2c17
Konstantin <nekotof@yandex.ru>
2025-05-29 22:36:19 +0500

    DEBUG TRUE

--- cenometr/settings.py
+++ cenometr/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['5.129.195.180', 'localhost', 'cenometr.online']
 AUTH_USER_MODEL = 'catalog.User'
